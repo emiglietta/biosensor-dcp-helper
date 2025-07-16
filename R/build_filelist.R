19d2c24d99613e2a3159bcea86829c24bb1d5bc7
emiglietta <esteban.miglietta@gmail.com>
2025-07-16 13:58:37 +0000

    adapt build_filelist to new pe2loaddata

--- R/build_filelist.R
+++ R/build_filelist.R
@@ -12,10 +12,19 @@
 #' @import reticulate
 #'
 #' @examples
+<<<<<<< Updated upstream
 build_filelist <- function(path, force, path_base, path_yml="~/mcsaba/biosensor/src/dcp_helper/python/pe2loaddata_config.yml"){
   # path mus be with trailing backslash
 
   # parent <- path %>% str_split(pattern = "/") %>% unlist() %>% .[length(.)-1]
+=======
+build_filelist <- function(path, force, path_base, path_yml){
+
+#  path = "/home/ubuntu/bucket/inbox_mit/000012128303__2025-06-17T10_53_56-Measurement_1/Images/"
+#  force = TRUE
+#  path_base = "~/output.csv"
+#  path_yml = "~/dcp_helper_csaba/python/pe2loaddata_config_000012128303.yml"
+>>>>>>> Stashed changes
 
   if (file.exists(file.path(path_base, "loaddata_output.csv")) & force == FALSE){
     print("Found file list")
@@ -23,11 +32,29 @@ build_filelist <- function(path, force, path_base, path_yml="~/mcsaba/biosensor/
     # TODO: re-add code if Index.idx.xml does not exist
     print("Initiating file list")
 
+<<<<<<< Updated upstream
     system(paste("python2",
                  "~/projects/biosensor/src/biosensor-dcp-helper/python/pe2loaddata.py",
                  paste0("--index-directory=", path),
                  path_yml,
                  file.path(path_base, "loaddata_output.csv") ) )
+=======
+#### The new version of the Operetta index file (June 2025) requires the use of an updated version of pe2loaddata, which requires python 3.8 and has a different call
+
+   # system(paste("python2",
+   #              "~/dcp_helper_csaba/python/pe2loaddata.py",
+   #              paste0("--index-directory=", path),
+   #              path_yml,
+   #              file.path(path_base, "loaddata_output.csv") ) )
+   system(paste("python3.8",
+                "-m",
+		"pe2loaddata",
+                paste0("--index-directory=", path),
+                paste0("--index-file=", paste(path, "Index.xml", sep="/")),
+                path_yml,
+                file.path(path_base, "loaddata_output.csv"),
+                "--search-subdirectories"))
+>>>>>>> Stashed changes
   }
 
   print("Reading file list")
@@ -36,3 +63,4 @@ build_filelist <- function(path, force, path_base, path_yml="~/mcsaba/biosensor/
 
   return(dir_content)
 }
+
