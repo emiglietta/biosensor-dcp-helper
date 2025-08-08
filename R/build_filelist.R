eb1ef471fc6bb89249bd58368bb2b0439759867a
emiglietta <esteban.miglietta@gmail.com>
2025-08-08 15:02:48 -0700

    Update build_filelist.R

--- R/build_filelist.R
+++ R/build_filelist.R
@@ -21,12 +21,14 @@ build_filelist <- function(path, force, path_base, path_yml){
   } else {
     # TODO: re-add code if Index.idx.xml does not exist
     print("Initiating file list")
+
     system(paste("python3.8 -m pe2loaddata",
                 "--index-directory", path,
                 "--index-file", file.path(path, "Index.xml"),
                 path_yml,
                 file.path(path_base, "loaddata_output.csv"),     
-                "--search-subdirectories"))
+                "--search-subdirectories",
+                "; echo 'Finished running pe2loaddata'"))
   }
 
   print("Reading file list")
