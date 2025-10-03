13c4e190a1e0d3a0eb7f1d87416b5d8532722bb4
emiglietta <esteban.miglietta@gmail.com>
2025-10-03 15:57:33 -0700

    adapt to recent changes in the DB structure
    staining_layout_version in the measurement table is now staining_template_name
    remove staining_layout_id from the measurements table
    staining_layout table is now called legacy_staining_layout

--- vignettes/aggregate_manuscript-202505_data.R
+++ vignettes/aggregate_manuscript-202505_data.R
@@ -12,17 +12,17 @@ library(yaml)
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args) < 3) {
-  stop(paste0(length(args), " arguments were provided. Usage: Rscript aggregate_data.R <session_id> <yml_path> <staining_layout_version>"), call. = FALSE)
+  stop(paste0(length(args), " arguments were provided. Usage: Rscript aggregate_data.R <session_id> <yml_path> <staining_template_name>"), call. = FALSE)
 }
 
 # session_id is the name of the specific 'reading' of the plate, i.e. "000012126003__2025-01-16T14_31_24-Measurement_1"
 session_id = args[1] # takes the value from the first argument when calling the function from CLI
 yml_path = args[2] # 2nd argument, path to the yaml file containing the different options for the staining layout
-staining_layout_version = args[3] #3rd argument, version of the staining layout
+staining_template_name = args[3] #3rd argument, version of the staining layout, aka, staining template name
 
 # session_id = "000012126003__2025-01-16T14_31_24-Measurement_1"
 # yml_path = "dcp_helper_csaba/vignettes/staining_layouts.yml"
-# staining_layout_version = "v2"
+# staining_template_name = "v2"
 
 # Validate YAML file exists
 if (!file.exists(yml_path)) {
@@ -104,7 +104,7 @@ read_and_merge_measurements <- function(result_path, pattern = "*Cells.csv"){
   }
 }
 
-get_validated_channels <- function(yml_path, staining_layout_version) {
+get_validated_channels <- function(yml_path, staining_template_name) {
   # Read and parse YAML file
   yaml_data <- yaml.load_file(yml_path)
   
@@ -113,17 +113,17 @@ get_validated_channels <- function(yml_path, staining_layout_version) {
     yaml_data <- list(yaml_data)
   }
   
-  # Find the requested staining_layout_version
+  # Find the requested staining_template_name
   layout <- NULL
   for (item in yaml_data) {
-    if (is.list(item) && !is.null(item$staining_layout) && item$staining_layout == staining_layout_version) {
+    if (is.list(item) && !is.null(item$staining_template_name) && item$staining_template_name == staining_template_name) {
       layout <- item
       break
     }
   }
   
   if (is.null(layout)) {
-    stop(paste("Staining layout version", staining_layout_version, "not found in YAML"), call. = FALSE)
+    stop(paste("Staining template name", staining_template_name, "not found in YAML"), call. = FALSE)
   }
   
   # Required channels
@@ -227,7 +227,7 @@ toc()
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------
 # Extract the highest staining_layout_id from the database to inform what id to start from when assigning to new wells
-staining_layout <- tbl(pool.manuscript202505, "staining_layout")
+staining_layout <- tbl(pool.manuscript202505, "legacy_staining_layout")
 
 max_id <- staining_layout %>%
   summarise(max_id = max(staining_layout_id, na.rm = TRUE)) %>%
@@ -253,12 +253,10 @@ new_measurement = tibble(sample_id = session_id %>% str_extract(pattern = "0000\
     separate(measurement_id, remove = FALSE, sep = "-", c("t1", "t2", "t3", "measurement_descriptor", "timepoint_descriptor", "well", "field_descriptor", "channel")) %>%
     select(-(t1:t3)) %>% # drop t1, t2, t3
     select(-(channel)) %>% # drop channel
-    mutate(measurement_descriptor = str_extract(measurement_descriptor, pattern = "\\d") %>% as.numeric(),
+    mutate(measurement_descriptor = str_extract(measurement_descriptor, pattern = "\\d+") %>% as.numeric(),
            timepoint_descriptor = str_extract(timepoint_descriptor, pattern = "\\d+") %>% as.numeric()) %>%
     mutate(local_path = results_list) %>% 
-    #############################################
-    # this is adding 1 to the id for each image, but i need to add for each well!!!
-    mutate(staining_layout_id = max_id + dense_rank(well)) %>% # add sequentially incrementing numbers for each well, based on the last satining_layout_id listed in the DB
+    mutate(staining_template_name = staining_template_name) %>% # staining tamplate name provided in arguments
     rowwise() %>%
     mutate(measurement_checksum = compute_measurement_checksum(local_path %>% as.character()) %>% as.character()) %>%
     anti_join(existing_measurement)
@@ -266,7 +264,7 @@ toc()
 
 # Extract channel list from the yml file, based on the version indicated whren calling this script
 # TODO: CURRENT VERSION ASSUMES ONE STAINING LAYOUT PER PLATE (SESSION_ID), IT MIGHT NOT BE THIS WAY IN THE FUTURE!
-staining_layout_channels <- staining_layout_channels <- get_validated_channels(yml_path, staining_layout_version = staining_layout_version)
+staining_layout_channels <- get_validated_channels(yml_path, staining_template_name = staining_template_name)
 
 new_staining_layout <- new_measurement %>%
   mutate(row = match(str_extract(well, "[A-Z]"), LETTERS),   # Convert row letter to number (A=1, B=2, ..., Z=26)
@@ -274,7 +272,7 @@ new_staining_layout <- new_measurement %>%
   select(well,row,col,staining_layout_id) %>%
   distinct(well, .keep_all = TRUE) %>%
   mutate(
-    staining_layout = staining_layout_version,
+    staining_layout = staining_template_name,
     ch1 = staining_layout_channels$ch1,
     ch2 = staining_layout_channels$ch2,
     ch3 = staining_layout_channels$ch3,
